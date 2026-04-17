327d34fb315ad2d61ce2c6b7efcd1b0c1da95ee4
candientuthongminh <qtnglobal.com@gmail.com>
2026-04-17 15:50:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+async function loadProducts(){
+  const res = await fetch("data/products.json");
+  const data = await res.json();
+
+  const list = document.getElementById("productList");
+  const menu = document.getElementById("menuProducts");
+
+  data.forEach(p => {
+    list.innerHTML += `
+      <div class="product" onclick="goDetail(${p.id})">
+        <img src="${p.image}">
+        <p>${p.name}</p>
+      </div>
+    `;
+    menu.innerHTML += `<div onclick="goDetail(${p.id})">${p.name}</div>`;
+  });
+}
+
+function goDetail(id){
+  window.location.href = "product.html?id=" + id;
+}
+
+loadProducts();
