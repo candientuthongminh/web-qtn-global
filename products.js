1219156ba62fd4a7dd8cc5beb2461a6f22f1ea33
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-04 14:20:12 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,13 +1,12 @@
-const products = [
-  {
-    name: "Cân điện tử Jadever JWL",
-    slug: "jadever-jwl",
-    desc: "Cân điện tử chất lượng cao",
-    specs: ["3kg","6kg","15kg","30kg"],
-    images: [
-      "images/jwl/main.jpg",
-      "images/jwl/1.jpg",
-      "images/jwl/2.jpg"
-    ]
-  }
+const products=[
+{
+name:"Cân điện tử Jadever JWL",
+slug:"jadever-jwl",
+desc:"Cân điện tử chất lượng cao",
+specs:["3kg","6kg","15kg","30kg"],
+images:[
+"images/jwl/main.jpg",
+"images/jwl/1.jpg"
+]
+}
 ];
