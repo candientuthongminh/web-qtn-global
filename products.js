3960a837005a25d8c65c93978c3d5d7a5c28825a
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-04 14:27:47 +0700

    Update products.js

--- products.js
+++ products.js
@@ -8,5 +8,16 @@ images:[
 "images/jwl/main.jpg",
 "images/jwl/1.jpg"
 ]
+},
+  {
+  name: "Cân điện tử OHAUS",
+  slug: "ohaus",
+  desc: "Cân điện tử OHAUS chính hãng, độ chính xác cao",
+  specs: ["3kg", "6kg", "15kg"],
+  images: [
+    "images/ohaus/main.jpg",
+    "images/ohaus/1.jpg",
+    "images/ohaus/2.jpg"
+  ]
 }
 ];
