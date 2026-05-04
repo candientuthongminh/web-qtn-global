98e08b3a234cd8b3bb8fab74da7d75a23de8db5f
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-04 14:06:49 +0700

    Update products.js

--- products.js
+++ products.js
@@ -3,23 +3,11 @@ const products = [
     name: "Cân điện tử Jadever JWL",
     slug: "jadever-jwl",
     desc: "Cân điện tử chất lượng cao",
-    folder: "jwl",
-    specs: ["3kg", "6kg", "15kg", "30kg"],
+    specs: ["3kg","6kg","15kg","30kg"],
     images: [
       "images/jwl/main.jpg",
       "images/jwl/1.jpg",
       "images/jwl/2.jpg"
-    ]
-  },
-  {
-    name: "Cân điện tử Vibra HAW",
-    slug: "vibra-haw",
-    desc: "Cân kỹ thuật chính xác",
-    folder: "vibra-haw",
-    specs: ["3kg", "6kg"],
-    images: [
-      "images/vibra-haw/main.jpg",
-      "images/vibra-haw/1.jpg"
     ]
   }
 ];
