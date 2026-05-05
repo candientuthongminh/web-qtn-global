39715fb88ee451998672ea2dd88be43cd88a65e0
candientuthongminh <qtnglobal.com@gmail.com>
2026-05-05 16:14:16 +0700

    Update products.js

--- products.js
+++ products.js
@@ -3,14 +3,14 @@ const products = [
     name: "Cân điện tử Jadever JWL",
     slug: "jadever-jwl",
     image_folder: "jwl",
-    description: "Cân điện tử chất lượng cao",
+    description: "Jadever-JWL với thiết kế màn hình hiển thị số tinh thể lỏng LCD độ phân giải hiển thị cao.",
     variants: ["3kg", "6kg", "15kg", "30kg"]
   },
   {
     name: "Cân điện tử đếm VIBRA ALC",
     slug: "vibra-alc",
     image_folder: "vibra-alc",
-    description: "Cân đếm chính xác cao",
+    description: "Cân điện tử VIBRA ALC là dòng cân đếm nổi tiếng của hãng Vibra Nhật Bản. Cân điện tử ALC đạt độ chính xác tiêu chuẩn quốc tế, thiết kế chống bụi, thân thiện với người dùng. Cân ALC có nhiều ưu điểm nổi bật, giá thành hấp dẫn nên được lựa chọn sử dụng trong các ngành sản xuất, chế tạo, y tế, giáo dục…",
     variants: ["3kg", "6kg", "15kg"]
   }
 ];
